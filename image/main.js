4d1a1b3c7632a8efe042b5f81cc4f47ad11bee74
Rohan Dafare <121180382+rohandafarefare@users.noreply.github.com>
2023-01-16 22:44:49 +0530

    Add files via upload

---
+++ image/main.js
@@ -0,0 +1,21 @@
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbw8Yt5drWuJDDsaSXdix0XEypzk6CObBu1BgFxEvO5bMzEUTwFo6M5O7pi6xweYd8w/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById("msg")
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Sent"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+    });
+
+    var loader = document.getElementById("preloader");
+    window.addEventListener("load", function(){
+        loader.style.display = "none";
+    })
